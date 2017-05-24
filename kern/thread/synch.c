e2a72fa6139e62658a07aaf6823d60afb67143fe
yandong <pigheadzyd@gmail.com>
2017-05-24 14:11:13 -0400

    fifth

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -201,7 +201,7 @@ lock_acquire(struct lock *lock)
         // Write this
         spinlock_acquire(&lock->lk_spin);
         while(lock->hold){
-            wchan.lock(lock->lk_wc);
+            wchan_lock(lock->lk_wc);
             spinlock_release(lock->lk_spin);
             wchan_sleep(lock->lk_wc);
             spinlock_acquire(&lock->lk_spin);
